0875ba1366de148a73a9722f628be7f35f455607
BuildLowCode <103343964+BuildLowCode@users.noreply.github.com>
2022-05-03 22:07:44 -0500

    Update submission_created.js

--- .netlify/functions/submission_created.js
+++ .netlify/functions/submission_created.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 // Handle the lambda invocation
 exports.handler = async function (event, context, callback) {
     try {
-        const STACKBIT_CONTACT_FORM_SUBMISSION_URL = '/.netlify/functions/submission_created'
+        const STACKBIT_CONTACT_FORM_SUBMISSION_URL = '/.netlify/functions/submission_created';
         const url = process.env.STACKBIT_CONTACT_FORM_SUBMISSION_URL;
 
         if (!url) {
